282c3563f83634902216bc6af0b8a58bc8324387
kmohebbizadeh <67389272+kmohebbizadeh@users.noreply.github.com>
2022-10-23 18:23:18 -0400

    Delete iris_rec.py

--- iris_recognition.py
+++ iris_recognition.py
@@ -18,27 +18,6 @@ class iris_detection():
         else:
             return True
     def iris_localization(self):
-        localization_image = cv2.equalizeHist(self._img_gray)
-        localization_image = cv2.medianBlur(localization_image,7)
-
-        pupil_edges = cv2.Canny(localization_image,50,70)
-
-        pupil_circle = cv2.HoughCircles(pupil_edges,cv2.HOUGH_GRADIENT,1,20,
-                                    param1=50,param2=40,minRadius=10,maxRadius=70)
-
-        iris_edges = cv2.Canny(localization_image, 70, 150)
-        iris_circle = cv2.HoughCircles(iris_edges,cv2.HOUGH_GRADIENT,1,40,
-                                    param2=35,minRadius=20,maxRadius=120)
-
-        circles = np.append(pupil_circle, iris_circle, axis=0)
-        circles = np.uint16(np.around(circles))
-
-        for circle in circles:
-            for value in circle:
-                center_x = value[0]
-                center_y = value[1]
-                radius = value[2]
-                cv2.circle(localization_image,(center_x,center_y),radius,color = (0,255, 0), thickness = 2)
 
     def iris_normalization(self):
 
--- iris_test.py
+++ iris_test.py
@@ -18,13 +18,13 @@ test = cv2.GaussianBlur(test,(15,15), 0)
 edges = cv2.Canny(test,50,100)
 pupil = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,2, minDist = 70,
                             param2=50,minRadius=20,maxRadius=60)
-# print(pupil)
+
 pupil = np.uint16(np.around(pupil))
 for value in pupil[0,:]:
     center_x = value[0]
     center_y = value[1]
     radius = value[2]
-    # cv2.circle(image,(center_x,center_y),radius,color = (0,255, 0), thickness = 2)
+
 pupil = np.array([center_x, center_y, radius])
 base = np.zeros_like(test)
 mask_pupil = cv2.circle(base, (pupil[0], pupil[1]), pupil[2], (255,255,255), -1)
@@ -38,11 +38,11 @@ result = cv2.bitwise_and(result, mask_background)
 # cv2.waitKey(0)
 test = cv2.equalizeHist(result)
 test = cv2.GaussianBlur(test,(25,25),0)
-cv2.imshow('detected circles',test)
-cv2.waitKey(0)
+# cv2.imshow('detected circles',test)
+# cv2.waitKey(0)
 edges = cv2.Canny(test, 40, 70)
-cv2.imshow('detected circles',edges)
-cv2.waitKey(0)
+# cv2.imshow('detected circles',edges)
+# cv2.waitKey(0)
 iris = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,2,minDist = 1000,
                             param2=10,minRadius=pupil[2]+20,maxRadius=((pupil[2]*3)-10))
 print(iris)
@@ -56,17 +56,6 @@ cv2.circle(image,(iris[0],iris[1]),iris[2],color = (0,255, 0), thickness = 2)
 cv2.circle(image,(pupil[0],pupil[1]),pupil[2],color = (0,255, 0), thickness = 2)
 cv2.imshow('detected circles',image)
 cv2.waitKey(0)
-# circles = np.append(pupil, iris, axis=0)
-# circles = np.uint16(np.around(circles))
-#
-# for circle in circles:
-#     for value in circle:
-#         center_x = value[0]
-#         center_y = value[1]
-#         radius = value[2]
-#         cv2.circle(image,(center_x,center_y),radius,color = (0,255, 0), thickness = 2)
-#
-# cv2.imshow('detected circles',image)
-# cv2.waitKey(0)
+
 
 
