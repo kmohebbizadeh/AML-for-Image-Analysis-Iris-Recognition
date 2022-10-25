4f73633bcbd02b9fd55e48dfde68a62804933dda
kmohebbizadeh <67389272+kmohebbizadeh@users.noreply.github.com>
2022-10-25 19:07:56 -0400

    adjusting the iris recognition

--- iris_test.py
+++ iris_test.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-image = cv2.imread('test_2.bmp')
+image = cv2.imread('test_3.bmp')
 
 
 test = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -30,15 +30,15 @@ mask_background = cv2.circle(base, (pupil[0], pupil[1]), pupil[2]*3, (255,255,25
 result = cv2.bitwise_and(result, mask_background)
 
 test = cv2.equalizeHist(result)
-test = cv2.GaussianBlur(test,(25,25),0)
+test = cv2.GaussianBlur(test,(15,15),0)
 
-# cv2.imshow('detected circles',test)
-# cv2.waitKey(0)
+cv2.imshow('detected circles',test)
+cv2.waitKey(0)
 
-edges = cv2.Canny(test, 40, 70)
+edges = cv2.Canny(test, 50, 60)
 
-# cv2.imshow('detected circles',edges)
-# cv2.waitKey(0)
+cv2.imshow('detected circles',edges)
+cv2.waitKey(0)
 
 iris = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,2,minDist = 1000,
                             param2=10,minRadius=pupil[2]+20,maxRadius=((pupil[2]*3)-10))
@@ -50,11 +50,11 @@ for value in iris[0,:]:
     radius = value[2]
 iris = np.array([center_x, center_y, radius])
 
-# cv2.circle(image,(iris[0],iris[1]),iris[2],color = (0,255, 0), thickness = 2)
-# cv2.circle(image,(pupil[0],pupil[1]),pupil[2],color = (0,255, 0), thickness = 2)
+cv2.circle(image,(iris[0],iris[1]),iris[2],color = (0,255, 0), thickness = 2)
+cv2.circle(image,(pupil[0],pupil[1]),pupil[2],color = (0,255, 0), thickness = 2)
 
-# cv2.imshow('detected circles',image)
-# cv2.waitKey(0)
+cv2.imshow('detected circles',image)
+cv2.waitKey(0)
 
 base = np.zeros_like(image)
 mask_pupil = cv2.circle(base, (pupil[0], pupil[1]), pupil[2], (255,255,255), -1)
@@ -63,7 +63,7 @@ result = cv2.bitwise_and(image, mask_pupil)
 mask_iris = cv2.circle(base, (iris[0], iris[1]), iris[2], (255,255,255), -1)
 result = cv2.bitwise_and(result, mask_iris)
 
-# cv2.imshow('detected circles',result)
-# cv2.waitKey(0)
+cv2.imshow('detected circles',result)
+cv2.waitKey(0)
 
 
