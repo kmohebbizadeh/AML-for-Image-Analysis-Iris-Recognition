ef547c79861b5d864907c9c4ffab9dbc03cec97e
Ethan <59672771+etevanss@users.noreply.github.com>
2022-10-30 14:36:52 -0400

    added noise reduction and feature extraction functions

--- iris_recognition.py
+++ iris_recognition.py
@@ -9,7 +9,8 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-class iris_detection():
+
+class iris_detection:
     def __init__(self, image_path):
         self._img = None
         self._img_og = None
@@ -20,6 +21,7 @@ class iris_detection():
         self._iris_center_x = None
         self._iris_center_y = None
         self._iris_radius = None
+
     def load_image(self):
         self._img = cv2.imread(self._img_path)
         if type(self._img) is type(None):
@@ -27,27 +29,47 @@ class iris_detection():
         else:
             self._img_og = cv2.cvtColor(self._img, cv2.COLOR_BGR2GRAY)
             return True
+
     def iris_localization(self):
-        image = cv2.GaussianBlur(self._img,(15,15), 0)
-        edges = cv2.Canny(image,50,100)
-        pupil_edge = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,2, minDist = 70,
-                                    param2=50,minRadius=20,maxRadius=60)
+        image = cv2.GaussianBlur(self._img, (15, 15), 0)
+        edges = cv2.Canny(image, 50, 100)
+        pupil_edge = cv2.HoughCircles(
+            edges,
+            cv2.HOUGH_GRADIENT,
+            2,
+            minDist=70,
+            param2=50,
+            minRadius=20,
+            maxRadius=60,
+        )
 
         pupil_edge = np.uint16(np.around(pupil_edge))
-        for value in pupil_edge[0,:]:
+        for value in pupil_edge[0, :]:
             self._pupil_center_x = value[0]
             self._pupil_center_y = value[1]
             self._pupil_radius = value[2]
 
         blank = np.zeros_like(self._img_og)
-        mask_pupil = cv2.circle(blank, (self._pupil_center_x, self._pupil_center_y), self._pupil_radius, (255,255,255), -1)
+        mask_pupil = cv2.circle(
+            blank,
+            (self._pupil_center_x, self._pupil_center_y),
+            self._pupil_radius,
+            (255, 255, 255),
+            -1,
+        )
         mask_pupil = cv2.bitwise_not(mask_pupil)
         image = cv2.bitwise_and(self._img_og, mask_pupil)
-        mask_background = cv2.circle(blank, (self._pupil_center_x, self._pupil_center_y), self._pupil_radius*3, (255,255,255), -1)
+        mask_background = cv2.circle(
+            blank,
+            (self._pupil_center_x, self._pupil_center_y),
+            self._pupil_radius * 3,
+            (255, 255, 255),
+            -1,
+        )
         image = cv2.bitwise_and(image, mask_background)
 
         image = cv2.equalizeHist(image)
-        image = cv2.GaussianBlur(image,(15,15),0)
+        image = cv2.GaussianBlur(image, (15, 15), 0)
 
         # cv2.imshow('detected circles',test)
         # cv2.waitKey(0)
@@ -57,11 +79,18 @@ class iris_detection():
         # cv2.imshow('detected circles',edges)
         # cv2.waitKey(0)
 
-        iris_edge = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,2,minDist = 1000,
-                                    param2=10,minRadius=self._pupil_radius+20,maxRadius=((self._pupil_radius*3)-10))
+        iris_edge = cv2.HoughCircles(
+            edges,
+            cv2.HOUGH_GRADIENT,
+            2,
+            minDist=1000,
+            param2=10,
+            minRadius=self._pupil_radius + 20,
+            maxRadius=((self._pupil_radius * 3) - 10),
+        )
 
         iris_edge = np.uint16(np.around(iris_edge))
-        for value in iris_edge[0,:]:
+        for value in iris_edge[0, :]:
             self._iris_center_x = value[0]
             self._iris_center_y = value[1]
             self._iris_radius = value[2]
@@ -73,10 +102,22 @@ class iris_detection():
         # cv2.waitKey(0)
 
         blank = np.zeros_like(self._img_og)
-        mask_pupil = cv2.circle(blank, (self._pupil_center_x, self._pupil_center_y), self._pupil_radius, (255,255,255), -1)
+        mask_pupil = cv2.circle(
+            blank,
+            (self._pupil_center_x, self._pupil_center_y),
+            self._pupil_radius,
+            (255, 255, 255),
+            -1,
+        )
         mask_pupil = cv2.bitwise_not(mask_pupil)
         image = cv2.bitwise_and(self._img_og, mask_pupil)
-        mask_background = cv2.circle(blank, (self._iris_center_x, self._iris_center_y), self._iris_radius, (255,255,255), -1)
+        mask_background = cv2.circle(
+            blank,
+            (self._iris_center_x, self._iris_center_y),
+            self._iris_radius,
+            (255, 255, 255),
+            -1,
+        )
         image = cv2.bitwise_and(image, mask_background)
 
         # cv2.imshow('detected circles',result)
@@ -85,17 +126,29 @@ class iris_detection():
         self._img = image
 
     def iris_normalization(self, image):
-        theta = np.arange(0.00, np.pi * 2, 0.01) #array of columns in final image (goes to 6.28 because period is 2 pi)
-        r = np.arange(0, self._iris_radius - self._pupil_radius, 1) #array of rows in final image
-        final = image[self._iris_center_x - self._iris_radius : self._iris_center_x + self._iris_radius,
-                self._iris_center_y - self._iris_radius : self._iris_center_y + self._iris_radius]
+        theta = np.arange(
+            0.00, np.pi * 2, 0.01
+        )  # array of columns in final image (goes to 6.28 because period is 2 pi)
+        r = np.arange(
+            0, self._iris_radius - self._pupil_radius, 1
+        )  # array of rows in final image
+        final = image[
+            self._iris_center_x
+            - self._iris_radius : self._iris_center_x
+            + self._iris_radius,
+            self._iris_center_y
+            - self._iris_radius : self._iris_center_y
+            + self._iris_radius,
+        ]
         # cv2.imshow('detected circles',final)
         # cv2.waitKey(0)
-        cartesian_img = np.empty(shape = [self._iris_radius - self._pupil_radius,
-                                          self._iris_radius*2,
-                                          3]) # empty array of dimensions of final image
-        m = interp1d([np.pi * 2, 0],[0, self._iris_radius*2]) #interpolate location on x axis
-            # interpolation between x = values from 0 and 6.28 and y = 0 to width of final image
+        cartesian_img = np.empty(
+            shape=[self._iris_radius - self._pupil_radius, self._iris_radius * 2, 3]
+        )  # empty array of dimensions of final image
+        m = interp1d(
+            [np.pi * 2, 0], [0, self._iris_radius * 2]
+        )  # interpolate location on x axis
+        # interpolation between x = values from 0 and 6.28 and y = 0 to width of final image
 
         # calculate all pixel values for normalized cartesian image
         for z in r:
@@ -107,9 +160,8 @@ class iris_detection():
                 polarY = int((i * np.sin(j)) + self._iris_radius)
                 cartesian_img[z][int(m(j) - 1)] = final[polarY][polarX]
 
-
         # cartesian_img = cartesian_img[:][pupil[2]:iris[2]] #patch
-        cartesian_img = cartesian_img.astype('uint8')
+        cartesian_img = cartesian_img.astype("uint8")
         self._img = np.asarray(cartesian_img)
         # cv2.imshow('detected circles', img)
         # cv2.waitKey(0)
@@ -121,15 +173,98 @@ class iris_detection():
         image = cv2.equalizeHist(image)
         # enhanced_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # plt.imshow(enhanced_img)
-        self._img = image
+
+        # noise reduction
+        ret, eyelid = cv2.threshold(image, 90, 255, cv2.THRESH_BINARY)
+        # plt.imshow(eyelid)
+        eyelid = cv2.GaussianBlur(eyelid, (15, 15), 0)
+        eyelid = cv2.Canny(eyelid, 50, 100)
+        # plt.imshow(eyelid)
+
+        eyelid_circ = cv2.HoughCircles(
+            eyelid,
+            cv2.HOUGH_GRADIENT,
+            2,
+            minDist=500,
+            param2=10,
+            minRadius=0,
+            maxRadius=100,
+        )
+
+        eyelid_circ = np.uint16(np.around(eyelid_circ))
+        for value in eyelid_circ[0, :]:
+            eyelid_circ_x = value[0]
+            eyelid_circ_y = value[1]
+            eyelid_circ_radius = value[2]
+
+        # eyelid_circ
+
+        base = np.zeros_like(image)
+        mask_eyelid = cv2.circle(
+            base,
+            (eyelid_circ_x, eyelid_circ_y),
+            eyelid_circ_radius,
+            color=(0, 0, 0),
+            thickness=2,
+        )
+        res2 = cv2.bitwise_not(mask_eyelid)
+        # plt.imshow(res2)
+
+        mask_eyelid = cv2.bitwise_or(image, mask_eyelid)
+        result = cv2.bitwise_and(image, mask_eyelid)
+        mask_background = cv2.circle(
+            base,
+            (eyelid_circ_x, eyelid_circ_y),
+            eyelid_circ_radius,
+            (255, 255, 255),
+            -1,
+        )
+        result = cv2.bitwise_or(result, mask_background)
+        # plt.imshow(result)
+
+        ret, more_mask = cv2.threshold(result, 190, 255, cv2.THRESH_BINARY)
+        # plt.imshow(more_mask)
+
+        xd = cv2.bitwise_not(more_mask)
+        xd = cv2.bitwise_and(image, xd)
+        # plt.imshow(xd)
+        self._img = xd
 
     def feature_extraction(self):
-        pass
+        filter_size = 9
+        height = np.fix(filter_size / 2)
+        width = np.fix(filter_size / 2)
+        deltaX = 4.5
+        deltaY = 1.5
+        f = 1 / deltaY
+
+        # constructing the kernel
+        for i in range(int(-height), int(height) + 1, 1):
+            for j in range(int(-width), int(width) + 1, 1):
+                # normalizing_factor = (1 / (2 * np.pi * deltaX * deltaY))
+                g = np.exp(
+                    (-0.5) * ((i**2 / deltaX**2) + (j**2 / deltaY**2))
+                )  # Gabor function
+                m1 = np.cos(
+                    2 * np.pi * f * (np.sqrt((i**2) + (j**2)))
+                )  # modulating function for two channels
+                gabor_filter[i, j] = g * m1
+
+        # plt.imshow(gabor_filter)
+
+        # applying the kernel
+        filtered_img = cv2.filter2D(self._img, cv2.CV_8UC3, gabor_filter)
+        # plt.imshow(filtered_img)
+
+        # standardizing image shape for classifier input
+        standard_size_img = cv2.resize(filtered_img, (250, 60))
+        # plt.imshow(standard_size_img)
+        # standard_size_img.shape
+        return standard_size_img
 
     def dimensionality_reduction(self):
-        #return values
+        # return values
         pass
-
 
 
 def iris_recognition(path):
@@ -144,72 +279,70 @@ def iris_recognition(path):
     return values
 
 
-
-
 ################
-#Implementation
+# Implementation
 ################
 
-folder = 'CASIA Iris Image Database (version 1.0)'
-train_df = pd.DataFrame(columns=['id', 'eye1', 'eye2', 'eye3'])
-test_df = pd.DataFrame(columns=['id', 'eye1', 'eye2', 'eye3', 'eye4'])
+folder = "CASIA Iris Image Database (version 1.0)"
+train_df = pd.DataFrame(columns=["id", "eye1", "eye2", "eye3"])
+test_df = pd.DataFrame(columns=["id", "eye1", "eye2", "eye3", "eye4"])
 
 for person in os.listdir(folder):
-    train_info = {'id': person}
-    test_info = {'id': person}
-    for file in os.listdir(folder + '/' + person):
-        if file == '1':
-            for picture in os.listdir(folder + '/' + person + '/' + '1'):
-                if picture[-4:] != '.bmp':
+    train_info = {"id": person}
+    test_info = {"id": person}
+    for file in os.listdir(folder + "/" + person):
+        if file == "1":
+            for picture in os.listdir(folder + "/" + person + "/" + "1"):
+                if picture[-4:] != ".bmp":
                     continue
-                path = folder + '/' + person + '/' + '1' + '/' + picture
-                train_info['eye'+picture[-5]] = path
-        if file == '2':
-            for picture in os.listdir(folder + '/' + person + '/' + '2'):
-                if picture[-4:] != '.bmp':
+                path = folder + "/" + person + "/" + "1" + "/" + picture
+                train_info["eye" + picture[-5]] = path
+        if file == "2":
+            for picture in os.listdir(folder + "/" + person + "/" + "2"):
+                if picture[-4:] != ".bmp":
                     continue
-                path = folder + '/' + person + '/' + '2' + '/' + picture
-                test_info['eye'+picture[-5]] = path
+                path = folder + "/" + person + "/" + "2" + "/" + picture
+                test_info["eye" + picture[-5]] = path
 
     train_df = train_df.append(train_info, ignore_index=True)
     test_df = test_df.append(test_info, ignore_index=True)
 
 
-train_df = train_df.sort_values(by=['id'])
-test_df = test_df.sort_values(by=['id'])
+train_df = train_df.sort_values(by=["id"])
+test_df = test_df.sort_values(by=["id"])
 
-formatted_train_df = pd.DataFrame(columns=['id', 'image'])
-formatted_test_df = pd.DataFrame(columns=['id', 'image'])
+formatted_train_df = pd.DataFrame(columns=["id", "image"])
+formatted_test_df = pd.DataFrame(columns=["id", "image"])
 
 for index, row in train_df.iterrows():
-    id = row['id']
+    id = row["id"]
 
-    eye1 = iris_recognition(row['eye1'])
-    new_row = {'id': id, 'image': eye1}
+    eye1 = iris_recognition(row["eye1"])
+    new_row = {"id": id, "image": eye1}
     formatted_train_df = train_df.append(new_row, ignore_index=True)
 
-    eye2 = iris_recognition(row['eye2'])
-    new_row = {'id': id, 'image': eye2}
+    eye2 = iris_recognition(row["eye2"])
+    new_row = {"id": id, "image": eye2}
     formatted_train_df = train_df.append(new_row, ignore_index=True)
 
-    eye3 = iris_recognition(row['eye3'])
-    new_row = {'id': id, 'image': eye3}
+    eye3 = iris_recognition(row["eye3"])
+    new_row = {"id": id, "image": eye3}
     formatted_train_df = train_df.append(new_row, ignore_index=True)
 
 for index, row in test_df.iterrows():
-    id = row['id']
+    id = row["id"]
 
-    eye4 = iris_recognition(row['eye4'])
-    new_row = {'id': id, 'image': eye4}
+    eye4 = iris_recognition(row["eye4"])
+    new_row = {"id": id, "image": eye4}
     formatted_test_df = test_df.append(new_row, ignore_index=True)
 
-train_y = np.asarray(formatted_train_df['id'])
-train_x = formatted_train_df.drop(columns=['id'])
+train_y = np.asarray(formatted_train_df["id"])
+train_x = formatted_train_df.drop(columns=["id"])
 train_x = train_x.reshape((len(train_x), 1))
 train_y = train_y.reshape((len(train_y), 1))
 
-test_y = np.asarray(formatted_test_df['id'])
-test_x = np.asarray(formatted_test_df['id'])
+test_y = np.asarray(formatted_test_df["id"])
+test_x = np.asarray(formatted_test_df["id"])
 test_x = test_x.reshape((len(train_x), 1))
 test_y = test_y.reshape((len(train_y), 1))
 
@@ -218,17 +351,3 @@ model.fit(train_x, train_y)
 predictions = model.predit(test_x)
 
 accuracy = accuracy_score(train_y, predictions)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
